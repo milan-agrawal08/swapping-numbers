cfee354820f0b6ca4770cc567212d40eb8c46c34
milan-agrawal08 <71334541+milan-agrawal08@users.noreply.github.com>
2020-09-15 19:58:35 +0530

    Just swap it

---
+++ names c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	printf("enter any number");
+	int num=0;
+	scanf("%d",num);
+	printf("enter 2nd number");
+	int num2=0;
+	scanf("%d",num2);
+	printf("now the 1st number is %d and 2nd number is %d",num2,num);
+	return 0;
+}
